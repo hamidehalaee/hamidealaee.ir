2027a86547368acf42e5e0c80918c09c210f75e1
hamidealaee <hamidealaeeyasar@gmail.com>
2021-08-13 14:59:20 -0700

    add auto text writer

--- front/backend/app.js
+++ front/backend/app.js
@@ -5,6 +5,4 @@ app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-
-
-app.listen(3000,()=> console.log('connected port 3000'))
+app.listen(5000,()=> console.log('connected port 5000'))
--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,52 @@
+import './App.css'
+import Typewriter from 'typewriter-effect';
 function App() {
   return (
     <div className="App">
+
+      <div class="menu">
+
+        {/* <!-- <button type="button">ABOUT</button>
+        <div class="dropdown">
+
+           
+        </div> --> */}
+        <div class="dropdown">
+            <button type="button" class="dropbtn"><a href="/projects" style={{textDecoration: "none",color: "inherit"}}>PROJECTS</a></button>
+            {/* <!-- <div class="dropdown-contact">
+                <button type="button">GITHUB</button>
+                <button type="button">THIS SITE</button>
+            </div> --> */}
+        </div>
+        <div class="dropdown">
+            <button type="button" class="dropbtn"><a href="/contact"  style={{textDecoration: "none",color: "inherit"}}>CONTACT</a></button>
+            {/* <!-- <div class="dropdown-contact">
+                <button type="button">LINKEDIN</button>
+                <button type="button">EMAIL</button>
+                <button type="button">PHONE</button>
+            </div> --> */}
+        </div>
+        <div class="dropdown">
+            <button type="button" class="dropbtn"><a href="/contact"  style={{textDecoration: "none",color: "inherit"}}>RESUME</a></button>
+            {/* <!-- <div class="dropdown-contact">
+                <button type="button">RESUME</button>
+            </div> --> */}
+        </div>
+
+
+      </div>
+      <br />
+      
+      {/* <header style={{width:"100%" ,height: "100% "}}>
+      </header> */}
+      <br />
+      <Typewriter className="type"
+        options={{
+          strings: ['Hello World From Hamide Alaeeee'],
+          autoStart: true,
+          loop: true,
+        }}
+      />
     </div>
   );
 }

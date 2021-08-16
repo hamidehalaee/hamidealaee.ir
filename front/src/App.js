d1cf6ddfb37977fb66f50a9b43a94a74c74ccfee
hamidealaee <hamidealaeeyasar@gmail.com>
2021-08-16 02:15:23 -0700

    add education

--- front/src/App.js
+++ front/src/App.js
@@ -8,8 +8,9 @@ function App() {
       "type": "Javascript",
       "level": 50
     },
-    {"type": "Nodejs", "level": 30
-  },
+    {"type": "Node.js",
+     "level": 30
+    },
     {
       "type": "Docker",
       "level": 25
@@ -17,6 +18,18 @@ function App() {
     {
       "type": "NoSQL",
       "level": 12
+    },
+    {
+      "type": "SQL",
+      "level": 14
+    },
+    {
+      "type": "React.js",
+      "level": 12
+    },
+    {
+      "type": "Angular",
+      "level": 10
     }
   ]
   const colors = {
@@ -28,55 +41,52 @@ function App() {
   }
   return (
     <div className="App">
-
       <div class="menu">
-
-        {/* <!-- <button type="button">ABOUT</button>
-        <div class="dropdown">
-
-           
-        </div> --> */}
         <div class="dropdown">
             <button type="button" class="dropbtn"><a href="/projects" style={{textDecoration: "none",color: "inherit"}}>PROJECTS</a></button>
-            {/* <!-- <div class="dropdown-contact">
-                <button type="button">GITHUB</button>
-                <button type="button">THIS SITE</button>
-            </div> --> */}
         </div>
         <div class="dropdown">
             <button type="button" class="dropbtn"><a href="/contact"  style={{textDecoration: "none",color: "inherit"}}>CONTACT</a></button>
-            {/* <!-- <div class="dropdown-contact">
-                <button type="button">LINKEDIN</button>
-                <button type="button">EMAIL</button>
-                <button type="button">PHONE</button>
-            </div> --> */}
         </div>
         <div class="dropdown">
             <button type="button" class="dropbtn"><a href="/contact"  style={{textDecoration: "none",color: "inherit"}}>RESUME</a></button>
-            {/* <!-- <div class="dropdown-contact">
-                <button type="button">RESUME</button>
-            </div> --> */}
         </div>
-
-
       </div>
       <br />
-      
-      {/* <header style={{width:"100%" ,height: "100% "}}>
-      </header> */}
       <br />
             <Typewriter className="type" 
         options={{
-          strings: ['Hello World From Hamide Alaeeee','Hamideh is a software engineer and full-stack web developer using node.js in back-end and angular, react in front-end ','and it is nice to meet you.' ],
+          strings: ['Hello World From Hamideh Alaeeee','Hamideh is a software engineer and full-stack web developer using node.js in back-end and angular, react in front-end.','and it is nice to meet you.' ],
           autoStart: true,
-          loop: false,
+          loop: true,
         }}
       />
       <br />
-      <hr style={{height : "3px"}} />
+      <hr style={{height : "4px"}} />
+
+      <div style={{  
+  left: "150px",
+  width: "200px",
+  height: "100px",
+ }}>
       <h1>Ma Skills</h1>
-      <SkillBar skills={SKILLS} colors={colors} height={23}></SkillBar>
-      
+      </div>
+
+
+      <SkillBar skills={SKILLS} colors={colors} height={24}></SkillBar>
+      <hr style={{height : "4px"}} />
+      <div style={{  
+  left: "150px",
+  width: "200px",
+  height: "80px",
+ }}>
+
+      <h1>Educations</h1>
+      </div>
+      <h3 style={{  
+  left: "150px",width: "65%",}}>Islamic Azad University Branch Of Eslamshahr ,2019-2022</h3>
+      <p style={{  
+  left: "150px",width: "55%",}}>Bachelor Of Computer Science engineering of software.</p>
     </div>
   );
 }
